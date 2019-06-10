85667ecc0ae2cc37b9fd68f267b9f201789be500
Bill Tortorici <Bill.Tortorici@dsst.local>
2019-06-09 23:22:21 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to Bamazon!
+
+### This CLI application uses Node.js and MySQL to emulate an online shopping experience.
+
+#### How to Install and Run the Application
+
+1. Install the app by cloning this repo and navigating to the file in the command line.
+2. Remember to `npm install` so the dependencies are loaded to your local copy of the app.
+3. Create a database using MySQL called __Bamazon__.  Use the code in the `bamazonSchema.sql` file to populate your `Bamazon` database with tables and some inventory. (Use Workbench, Sequel Pro or MySQL in the command line).
+3. There is one dashboard to this application - __Customer View__. Run the application by typing `node bamazonCustomer.js` in the command line. Two new dashboards are coming soon.
+4. Follow the prompts to__Make Bamazon Purchases__.
---
+++ bamazonCustomer.js
@@ -0,0 +1,102 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Roscoe#1909",
+  database: "bamazon"
+});
+
+connection.connect(function (error) {
+  if (error) throw error;
+  afterConnection();
+
+});
+
+function afterConnection() {
+  connection.query("SELECT id, product, price FROM products", function (error, results) {
+    if (error) throw error;
+    console.table(results);
+    questionToUser();
+  });
+}
+
+function questionToUser() {
+  inquirer
+    .prompt([
+      {
+        name: "nameId",
+        type: "input",
+        message: "Enter the id of the product you want to purchase"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "Enter the quantity you want to purchase"
+      }
+
+    ])
+
+    .then(answers => {
+      connection.query(
+        "SELECT * FROM products WHERE?",
+        { id: answers.nameId },
+        function (error, results) {
+          if (error) throw error;
+          let price = results[0].price;
+
+          if (results[0].quantity >= answers.stock) {
+            let newAmountOfItems = results[0].quantity - answers.stock;
+
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  quantity: newAmountOfItems
+                },
+                {
+                  id: answers.nameId
+                }
+              ],
+              function (err) {
+                if (err) throw err;
+
+                var totalCost = price * answers.stock;
+                console.log(
+                  `Your cost is $ ${parseFloat(totalCost).toFixed(2)}`
+                );
+                continueShopping();
+              }
+            );
+          } else {
+            console.log("Insufficient quantity!");
+            continueShopping();
+          }
+        }
+      );
+    });
+}
+
+function continueShopping() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: " Would like to continue shopping?",
+      choices: ["Yes", "No"]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Yes":
+          questionToUser();
+          break;
+
+        case "No":
+          connection.end();
+          break;
+      }
+    });
+}
